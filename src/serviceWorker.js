ca51f7a407b972483ae3cfe28ae88f1b1f5cbf3b
Xalan Dames <115499157+damesx04@users.noreply.github.com>
2023-08-14 18:18:57 -0400

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,8 +1,6 @@
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
@@ -10,7 +8,6 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       return;
